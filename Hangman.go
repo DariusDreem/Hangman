ba8bbabfaf7eb1e5238b36912c1b4cd86699f4c8
faure <leo.faure@ynov.com>
2022-10-30 11:42:55 +0100

    add End sentence

--- Hangman.go
+++ Hangman.go
@@ -32,7 +32,7 @@ func main() {
 			attemps--
 			println("\nNot present in the word,", attemps, "attempts remaining\n")
 		}
-		//test j'espere que c bon  cette fois si j'en peu plu
 		indice = -1
 	}
+	println("t'es nul c'était :", mot)
 }
