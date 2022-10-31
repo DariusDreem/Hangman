b52bd7da0fc3d9256f2f5e48c0e434ab137dc8e4
Brun <sasha.brun@ynov.com>
2022-10-31 13:58:17 +0100

    First commit

--- test.go
+++ test.go
@@ -1,20 +1,8 @@
 package main
 
-import "fmt"
-
-func main() {
-	var liste []int
-	liste = append(liste, 6)
-	liste = append(liste, 2)
-	liste = append(liste, 5)
-	liste = append(liste, 7)
-	nbr := 5
-	val := search_nbr(liste, nbr)
-	fmt.Print(val)
-}
-func search_nbr(param []int, nbr int) bool {
-	for i := 0; i < len(param); i++ {
-		if param[i] == nbr {
+func verifie(liste []int, nbr int) bool {
+	for i := 0; i < len(liste); i++ {
+		if liste[i] == nbr {
 			return false
 		}
 	}
